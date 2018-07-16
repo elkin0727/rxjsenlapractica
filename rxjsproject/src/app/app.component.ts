6829eb00253c4edd5b1d453d652b5697260cc4c9
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-15 20:06:46 -0500

    Example differents streams

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  seconds = 0;
+
+  ngOnInit(){
+    setInterval(() => {
+      this.seconds++;
+      console.log(this.seconds); // indefinido
+    }, 1000);
+
+    setTimeout(() => {
+      console.log('Tiene un fin');
+    }, 3000)
+  }
+
+  onKeyPress(inputValue: KeyboardEvent){
+    console.log(inputValue); // indefinido
+  }
+
+  saveForm(event: MouseEvent){
+    console.log(event); // indefinido
+  }
 }
