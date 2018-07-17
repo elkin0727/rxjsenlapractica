5a420a2f84c7db2e308a213a8cd343471c3f7e74
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-17 06:48:17 -0500

    Improve concept defining events directly

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +10,25 @@ export class AppComponent implements OnInit {
   seconds = 0;
 
   ngOnInit(){
+    //stream indefinido
+    document.getElementById('saveButton').addEventListener('click', evt => {
+      console.log('click: ', evt);
+    });
+
+    //stream indefinido
+    document.getElementById('nameInput').addEventListener('keypress', evt => {
+      console.log('keypress: ',  evt);
+    });
+
+    // stream indefinido
     setInterval(() => {
       this.seconds++;
-      console.log(this.seconds); // indefinido
+      console.log(this.seconds); 
     }, 1000);
 
+    // stream con un complete, no vuelve a emitir
     setTimeout(() => {
       console.log('Tiene un fin');
     }, 3000)
-  }
-
-  onKeyPress(inputValue: KeyboardEvent){
-    console.log(inputValue); // indefinido
-  }
-
-  saveForm(event: MouseEvent){
-    console.log(event); // indefinido
   }
 }
