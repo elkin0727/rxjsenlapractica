22c4d240f8c6a85883f57ba615e13c37e15ece42
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-17 08:12:42 -0500

    Use the unsubscription method

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, interval, timer } from 'rxjs';
+import { fromEvent, interval, timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -26,18 +26,32 @@ export class AppComponent implements OnInit {
     //   }, 3000);
     // });
 
-    //1. Comportamiento indefinido
-    const interval$ = interval(1000); 
-    //Un observable es solo como un template, un molde, solo explicar como queremos que se comporte
-
-    //2. tomar el molde y un stream que se comporte tal cual
-    interval$.subscribe(val => console.log("stream 1 ", val));
-
-    //3. Crear un observable de eventos del boton
+    // 5. Menos mal, estamos llenos de "utilitarios" que ya tienen comportamientos que necesitamos
+    // y este es solo un ejemplo, vamos a hacer un ejemplo
+    const interval$ = timer(3000, 1000);
     const click$ = fromEvent(document.getElementById('saveButton'), 'click');
+    let subscriptionInterval: Subscription;
 
-    //4. Crear la instancia
-    click$.subscribe(evt => console.log(evt));
+
+    click$.subscribe(
+      evt => {
+        console.log(evt);
+        subscriptionInterval = interval$.subscribe(val => this.seconds++)
+
+        // 6. Ahora supongamos que el requerimiento cambia un poquito,
+        // y el contador debe parar a los 3 segundos. 
+        // Aprovechemos para explicar el siguiente concepto del metodo subscribe
+        // como su nombre indica así como hay subscribe, podríamos tambien desuscribirnos? si!
+        setTimeout(() => {
+          subscriptionInterval.unsubscribe();
+        }, 6000);
+        
+      },
+      err => console.log(err), // example, fetch error when get info from backend
+      () => console.log('completed') // the stream has completed
+    );
+
+
 
   }
 }
