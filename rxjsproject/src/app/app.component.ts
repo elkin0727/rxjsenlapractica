ce21ccd5bd412547b64a0cbad40da7387667c1c6
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-17 10:14:22 -0500

    Remove comments

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -11,37 +11,15 @@ export class AppComponent implements OnInit {
   seconds = 0;
 
   ngOnInit() {
-    // //stream indefinido
-    // document.getElementById('saveButton').addEventListener('click', evt => {
-    //   console.log('click: ', evt);
-    //   // stream con un complete, no vuelve a emitir
-    //   setTimeout(() => {
-    //     console.log('Tiene un fin');
-    //     // stream indefinido
-    //     setInterval(() => {
-    //       this.seconds++;
-    //       console.log(this.seconds);
-    //     }, 1000);
-
-    //   }, 3000);
-    // });
-
-    // 5. Menos mal, estamos llenos de "utilitarios" que ya tienen comportamientos que necesitamos
-    // y este es solo un ejemplo, vamos a hacer un ejemplo
     const interval$ = timer(3000, 1000);
     const click$ = fromEvent(document.getElementById('saveButton'), 'click');
     let subscriptionInterval: Subscription;
 
-
     click$.subscribe(
       evt => {
         console.log(evt);
-        subscriptionInterval = interval$.subscribe(val => this.seconds++)
+        subscriptionInterval = interval$.subscribe(val => this.seconds++);
 
-        // 6. Ahora supongamos que el requerimiento cambia un poquito,
-        // y el contador debe parar a los 3 segundos. 
-        // Aprovechemos para explicar el siguiente concepto del metodo subscribe
-        // como su nombre indica así como hay subscribe, podríamos tambien desuscribirnos? si!
         setTimeout(() => {
           subscriptionInterval.unsubscribe();
         }, 6000);
@@ -50,8 +28,6 @@ export class AppComponent implements OnInit {
       err => console.log(err), // example, fetch error when get info from backend
       () => console.log('completed') // the stream has completed
     );
-
-
-
+    
   }
 }
