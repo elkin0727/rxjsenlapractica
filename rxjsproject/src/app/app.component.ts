32bab13c57856a87ba1f5ecb2aaaf6661291771a
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-17 07:09:51 -0500

    Callback hell

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -9,21 +9,24 @@ export class AppComponent implements OnInit {
 
   seconds = 0;
 
-  ngOnInit(){
+  ngOnInit() {
     //stream indefinido
     document.getElementById('saveButton').addEventListener('click', evt => {
       console.log('click: ', evt);
+      // stream con un complete, no vuelve a emitir
+      setTimeout(() => {
+        console.log('Tiene un fin');
+        // stream indefinido
+        setInterval(() => {
+          this.seconds++;
+          console.log(this.seconds);
+        }, 1000);
+
+      }, 3000);
     });
 
-    // stream indefinido
-    setInterval(() => {
-      this.seconds++;
-      console.log(this.seconds); 
-    }, 1000);
 
-    // stream con un complete, no vuelve a emitir
-    setTimeout(() => {
-      console.log('Tiene un fin');
-    }, 3000)
+
+
   }
 }
