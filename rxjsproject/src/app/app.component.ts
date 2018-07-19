701bfb985ecf0dc2fa82f7b94d53bd6a553b6f9d
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-19 07:22:54 -0500

    step02

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { concat, fromEvent, interval, timer, Subscription, noop, Observable, of, from } from 'rxjs';
 import { map, filter, shareReplay, tap, delay, zip, concatMap } from 'rxjs/operators';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +10,17 @@ import { map, filter, shareReplay, tap, delay, zip, concatMap } from 'rxjs/opera
 export class AppComponent implements OnInit {
 
   ngOnInit() {
+    document.getElementById('startButton').addEventListener('click', (event) => {
+      console.log(event);
+    });
 
-  }
-  
+    let seconds = 0;
+    setInterval(() => {
+      console.log(seconds++);
+    }, 1000);
+
+    setTimeout(() => console.log('Completed'), 4000);
+
+
+  }  
 }
