f029c2070e16cd1a1ea179ade8d69629c03eb978
Elkin Siabato <elkin.siabato@bizagi.com>
2018-07-19 07:10:18 -0500

    Start presentation

--- rxjsproject/src/app/app.component.ts
+++ rxjsproject/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { concat, fromEvent, interval, timer, Subscription, noop, Observable, of, from } from 'rxjs';
-
 import { map, filter, shareReplay, tap, delay, zip, concatMap } from 'rxjs/operators';
 
 
@@ -11,94 +10,8 @@ import { map, filter, shareReplay, tap, delay, zip, concatMap } from 'rxjs/opera
 })
 export class AppComponent implements OnInit {
 
-  // 1. Add lists where save the filer pokemons
-  levelOne$: Observable<Pokemon[]>;
-  levelTwo$: Observable<Pokemon[]>;
-
-  seconds = 0;
-
   ngOnInit() {
-    const interval$ = timer(3000, 1000);
-    const click$ = fromEvent(document.getElementById('saveButton'), 'click');
-    let subscriptionInterval: Subscription;
 
-    click$.subscribe(
-      evt => {
-        subscriptionInterval = interval$.subscribe(val => this.seconds++);
-
-        setTimeout(() => {
-          subscriptionInterval.unsubscribe();
-        }, 6000);
-
-      },
-      noop,
-      () => console.log('completed') // the stream has completed
-    );
-
-    //EXAMPLE TWO
-
-    const http$: Observable<Pokemon[]> = this.createHttpObservable('api/pokemons');
-
-    const pokemons$ = http$
-      .pipe(
-        tap(() => console.log("HTTP Executed")),
-        map(res => res["payload"]),
-        shareReplay()
-      );
-
-    this.levelOne$ = pokemons$
-      .pipe(
-        map(pokemons => pokemons
-          .filter(pokemon => pokemon.nivel === 1))
-      );
-
-    this.levelTwo$ = pokemons$
-      .pipe(
-        map(pokemons => pokemons
-          .filter(pokemon => pokemon.nivel === 2))
-      );
-
-
-    //EXAMPLE THREE
-    const obs1$:Observable<string> = of('a','b','c');
-    const obs2$:Observable<string> = of('d','e','f');
-    const obs3$:Observable<string> = of('g','h','i');
-    
-    var self = this;
-    const result$ = concat(obs1$, obs2$, obs3$).pipe(
-      zip(interval$),
-      tap(console.log),
-      concatMap((values) => {console.log('test', values);return self.createHTTPPutObservable.apply(self, values);})
-      //,
-      //filter((pokemon: string, second: number) => second % 2 !== 0)
-
-    );
-
-    result$.subscribe();
   }
-
-  createHttpObservable(url: string) {
-    return Observable.create(observer => {
-      fetch(url)
-        .then(response => { return response.json() })
-        .then(body => {
-          observer.next(body);
-          observer.complete();
-        })
-        .catch(err => {
-          observer.error(err);
-        });
-    });
-  }
-
-  createHTTPPutObservable(pokemonName: string, moment: number){
-    const sendData$ = from(fetch(`/api/Pokemon/${moment}/${pokemonName}`, {
-      method: 'PUT',
-      body: JSON.stringify({moment, pokemonName}),
-      headers: {
-        'content-type': 'application/json'
-      }
-    }));
-    return sendData$;
-  }
+  
 }
